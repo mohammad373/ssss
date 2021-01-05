3e67b3331cde8b04fa8b114de26a99d53666c278
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2021-01-05 11:35:11 +0330

    Update p.py

--- p.py
+++ p.py
@@ -25,13 +25,15 @@ def __target__():
         target = "http://" + target
     s1 = requests.get(target)
     if s1.status_code == 200:
-        print(Forre.GREEN + "[+] ~ Your Domain Is Found ;))"
+        print(Forre.GREEN + "[+] ~ Your Domain Is Found ;))")
     else:
-
+            try:
               time.sleep(1)
               print(Fore.RED = "[-] ~ Error : Your Domain Is Not Found ;(")
               time.sleep(1)
               sys.exit()
+            except:
+                sys.exit()
     s2 = requests.get( "https://api.hackertarget.com/whatweb/?q="  +  target).text
     print(Fore.GREEN + s2)
     time.sleep(1)
